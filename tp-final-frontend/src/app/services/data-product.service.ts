9fe41ec52e1cfaf59c8bf5c37c75eeec8842d6a8
Walter Gauto <waltergautofcb@gmail.com>
2021-11-18 22:08:23 -0300

    Add methos for product(getProducts, addProduct, removeProduct)

--- tp-final-frontend/src/app/services/data-product.service.ts
+++ tp-final-frontend/src/app/services/data-product.service.ts
@@ -1,9 +1,67 @@
 import { Injectable } from '@angular/core';
 
+import { Product } from '../models/product';
+
 @Injectable({
   providedIn: 'root'
 })
 export class DataProductService {
 
-  constructor() { }
+  products: Product[]; //array of products
+
+  constructor() {
+    this.products = [];
+  }
+
+  getProducts(): Product[] {
+    
+    if ( localStorage.getItem('products') === null ) {
+      
+      this.products = [];
+    
+    } else {
+
+      this.products = JSON.parse(localStorage.getItem('products') || '{}' );
+
+    }
+
+    return this.products;
+  
+  }
+
+  addProduct( product: Product ): void {
+
+    let products; 
+
+    this.products.push( product );
+
+    if ( localStorage.getItem('products') === null ) { //if array has not products yet
+      
+      products = [];
+      products.push( product );
+      localStorage.setItem( 'products', JSON.stringify( products ) );
+    
+    } else { //if array already products
+
+      products = JSON.parse( localStorage.getItem( 'products' ) || '{}');
+      products.push( product );
+      localStorage.setItem( 'products', JSON.stringify( products ) );
+
+    }
+
+  }
+
+  removeProduct( product: Product ) {
+
+      for ( let i = 0; i < this.products.length; i++ ) {
+
+        if ( product == this.products[i] ) {
+          
+          this.products.splice(i, 1);
+          localStorage.setItem( 'products', JSON.stringify( this.products ) );
+          
+        } 
+      }
+
+  }
 }
