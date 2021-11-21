1e34a703484c9b4b4e7a5072f410540cb22d2f1a
Walter Gauto <waltergautofcb@gmail.com>
2021-11-20 22:58:26 -0300

    Modify home page design

--- tp-final-frontend/src/app/components/home/home.component.ts
+++ tp-final-frontend/src/app/components/home/home.component.ts
@@ -17,7 +17,7 @@ export class HomeComponent implements OnInit {
     this.router.navigate(['/clients']);
   }
   
-  navigateToProductos( ) {
+  navigateToProducts( ) {
     this.router.navigate(['/products']);
   }
 
